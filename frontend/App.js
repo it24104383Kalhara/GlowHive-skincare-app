111f9ab6e45048710077404a0df40a26974f6fd4
IT24104383 Kalhara K.V <vinodkalhara906@gmail.com>
2026-04-06 21:27:35 +0530

    chore: dependencies and base App implemented

--- frontend/App.js
+++ frontend/App.js
@@ -3,14 +3,18 @@ import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
 import AppNavigator from './src/navigation/AppNavigator';
+import { AuthProvider } from './src/contexts/AuthContext';
 
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <NavigationContainer>
-        <StatusBar style="dark" backgroundColor="#F2F0ED" />
-        <AppNavigator />
-      </NavigationContainer>
-    </SafeAreaProvider>
+    <AuthProvider>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <StatusBar style="dark" backgroundColor="#F2F0ED" />
+          <AppNavigator />
+        </NavigationContainer>
+      </SafeAreaProvider>
+    </AuthProvider>
   );
 }
+
